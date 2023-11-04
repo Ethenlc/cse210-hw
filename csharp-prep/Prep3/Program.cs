c7e6067dab5104a4456adc8ec8bfcbb39f8f1b1f
Ethenlc <cam22015@byui.edu>
2023-11-04 11:57:30 -0600

    Added prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1,101);
+
+        string input;
+        int guess;
+
+        do
+        {
+        Console.WriteLine("What is your guess? ");
+            input = Console.ReadLine();
+            guess = int.Parse(input);
+        
+        if (guess > magicNum)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < magicNum)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        } while (guess != magicNum);
+        
+        Console.WriteLine("Correct!");
     }
 }
