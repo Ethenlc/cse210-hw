b5dfa70dc2f541458b3238bc3d82e4f059e260ce
Ethenlc <cam22015@byui.edu>
2023-11-04 12:48:19 -0600

    Added prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        do
+        {
+            Console.WriteLine("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+            numbers.Add(input);
+
+        } while (input != 0);
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The biggest number is {max}");
     }
 }
