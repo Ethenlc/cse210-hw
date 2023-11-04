c3a6192512e01f869f481fa04691185d893f8567
Ethenlc <cam22015@byui.edu>
2023-11-04 13:05:28 -0600

    Added prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your username? ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int favNum)
+    {
+        return favNum * favNum;
+    }
+
+    static void DisplayResult(string username, int square)
+    {
+        Console.WriteLine($"{username}, the square of your number is {square}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string username = PromptUserName();
+
+        int favNum = PromptUserNumber();
+
+        int square = SquareNumber(favNum);
+
+        DisplayResult(username, square);
     }
 }
