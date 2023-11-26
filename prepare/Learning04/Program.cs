9a4942c0fff65d8e2581923e940794b6552806de
Ethenlc <cam22015@byui.edu>
2023-11-25 17:36:08 -0700

    Finished learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment testAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(testAssignment.GetSummary());
+
+        MathAssignment testAssignment2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(testAssignment2.GetSummary());
+        Console.WriteLine(testAssignment2.GetHomeworkList());
+
+        WritingAssignment testAssignment3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(testAssignment3.GetSummary());
+        Console.WriteLine(testAssignment3.GetWritingInformation());
     }
 }
